a062173058b780a8b175740fc79ef7ec90af4b50
Mohammed ALi <m.ali.othman@gmail.com>
2020-06-01 12:14:22 +0300

    Update goaccess.sh

--- goaccess.sh
+++ goaccess.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-/usr/bin/ps aux | grep goaccess | awk '{print $2}' | head -n 1 | xargs kill -9
-
-/usr/bin/rm -rf /home/userhome/public_html/report.html
-
-sleep 10
-
-/usr/bin/goaccess /var/log/nginx/access.log --log-format=COMBINED -o /home/userhome/public_html/report.html --real-time-html & disown 
+!#/bin/bash
+sudo goaccess /var/log/apache2/access.log --log-format=COMBINED -a -o /var/www/html/report.html
